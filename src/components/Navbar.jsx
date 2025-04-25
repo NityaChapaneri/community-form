d7b4aa7aec0772dadfc6e33c5d716140dc6c82d0
Nitya Chapaneri <nitchapaneri2020@gmail.com>
2025-04-25 11:40:43 +0530

    fix: added mobile responsiveness to navbar and layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+};
+
+
 const Navbar = () => {
   return (
     <nav className="navbar">
